ec1c602d376ddffca843520429ff3612744c4875
ghmayakyan <gevorg.hmayakyan@gmail.com>
2022-09-09 13:50:10 +0400

    Initial Commit

---
+++ wdtDnd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wdtDnd
+//
+//  Created by Gevorg Hmayakyan on 09.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wdtDnd/wdtDndApp.swift
@@ -0,0 +1,17 @@
+//
+//  wdtDndApp.swift
+//  wdtDnd
+//
+//  Created by Gevorg Hmayakyan on 09.09.22.
+//
+
+import SwiftUI
+
+@main
+struct wdtDndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
